5697d94f4b724b7a02a9020c6578c0fe3db91620
Николай <usenkko.nikolay@gmail.com>
2026-05-29 13:35:31 +0300

    Add a database context

---
+++ smart-diary-backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using smart_diary_backend.Models;
+
+namespace smart_diary_backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
